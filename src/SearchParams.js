2895f942d48e05c56044923494219c7046379572
mrangelg <rangel.madelin@gmail.com>
2020-06-05 11:00:10 -0500

    Using select with onBlur action

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("Dog");
 
   return (
     <div className="search-params">
-      <h1>{location}</h1>
+      <h1>
+        {location} - {animal}
+      </h1>
       <form>
         <label htmlFor="location">
           location
@@ -15,6 +19,21 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={(e) => setLocation(e.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option value="all">All</option>
+            {ANIMALS.map((animal) => (
+              <option value={animal} key={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
