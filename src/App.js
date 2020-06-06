81f5d51bd5259ddc964e443a396de8184b455517
mrangelg <rangel.madelin@gmail.com>
2020-06-06 16:54:25 -0500

    Using React.StrictMode

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
-    <div>
-      <h1 id="something-important">Adopt me!</h1>
-      <SearchParams />
-    </div>
+    <React.StrictMode>
+      <div>
+        <h1 id="something-important">Adopt me!</h1>
+        <SearchParams />
+      </div>
+    </React.StrictMode>
   );
 };
 
