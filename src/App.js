f6782022d055fac2e03e0721528217ec00b42940
mrangelg <rangel.madelin@gmail.com>
2020-06-10 16:22:54 -0500

    ThemeContext created and using Provider

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import { Router, Link } from "@reach/router";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
+  const theme = useState("darkred");
+
   return (
     <React.StrictMode>
-      <div>
-        <header>
-          <Link to="/">Adopt me!</Link>
-        </header>
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
-      </div>
+      <ThemeContext.Provider value={theme}>
+        <div>
+          <header>
+            <Link to="/">Adopt me!</Link>
+          </header>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </div>
+      </ThemeContext.Provider>
     </React.StrictMode>
   );
 };
---
+++ src/ThemeContext.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const ThemeContext = createContext(["oranged", () => {}]);
+
+export default ThemeContext;
