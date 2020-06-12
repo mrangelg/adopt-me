78d23ab2f85d004da52b1ac8c0837931e3b2f399
mrangelg <rangel.madelin@gmail.com>
2020-06-12 17:03:23 -0500

    Template literals with Emotion

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "@reach/router";
 import { css } from "@emotion/core";
 
-const NavBar = () => (
-  <header
-    css={css`
-      background-color: #333;
-      padding: 15px;
-    `}
-  >
-    <Link to="/">Adopt Me!</Link>
-    <span aria-label="logo" role="img">
-      🐶
-    </span>
-  </header>
-);
+const NavBar = () => {
+  const [padding, setPadding] = useState(5);
+  return (
+    <header
+      onClick={() => setPadding(padding + 5)}
+      css={css`
+        background-color: #333;
+        padding: ${padding}px;
+      `}
+    >
+      <Link to="/">Adopt Me!</Link>
+      <span
+        css={css`
+          font-size: 60px;
+        `}
+        aria-label="logo"
+        role="img"
+      >
+        🐶
+      </span>
+    </header>
+  );
+};
 
 export default NavBar;
